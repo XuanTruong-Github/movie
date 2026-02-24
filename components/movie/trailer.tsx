38e55565f420332bb4dc69eb9b878b0a3e5b29cd
truong-lx <truonglx@allinbyte.io>
2026-02-24 12:31:04 +0700

    updated

--- components/movie/trailer.tsx
+++ components/movie/trailer.tsx
@@ -1,22 +1,39 @@
-import { ComponentProps } from "react";
+"use client";
+import { ComponentProps, useEffect, useRef } from "react";
+import Artplayer from "artplayer";
 
 type Props = { movie: any } & ComponentProps<"div">;
 
 export default function Trailer({ movie, ...props }: Props) {
-  if (!movie.trailer_url) return null;
+  if (!movie.trailer_url)
+    return <p className="py-10 text-center">Trailer không khả dụng</p>;
+  const $playerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if ($playerRef.current === null) return;
+    const art = new Artplayer({
+      id: movie.trailer_url,
+      container: $playerRef.current,
+      url: movie.trailer_url,
+      poster: `${movie.APP_DOMAIN_CDN_IMAGE}/${movie.item.poster_url}`,
+      autoPlayback: true,
+      pip: true,
+      fullscreen: true,
+      fullscreenWeb: true,
+      lock: true,
+      type: "m3u8",
+      gesture: true,
+      autoOrientation: true,
+      airplay: true,
+      theme: "#f0b000",
+      lang: "en",
+    });
+    return () => art.destroy(false);
+  }, [movie]);
   return (
-    <div {...props}>
-      {/* <ReactPlayer
-        slot="media"
-        src={movie.trailer_url}
-        controls
-        tabIndex={-1}
-        playsInline
-        key={movie.trailer_url}
-        width="100%"
-        height="auto"
-        style={{ aspectRatio: "16/9", marginBottom: "24px" }}
-      /> */}
-    </div>
+    <div
+      ref={$playerRef}
+      {...props}
+      className="w-full aspect-video mb-6 rounded-xl overflow-hidden"
+    ></div>
   );
 }
