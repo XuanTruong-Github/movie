b66f34ad41c21062e755b182a53cc0179d93712a
truong-lx <truonglx@allinbyte.io>
2026-02-25 16:26:38 +0700

    updated

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -13,8 +13,22 @@ import {
   InputGroupAddon,
   InputGroupInput,
 } from "../ui/input-group";
-import { Search } from "lucide-react";
+import { Menu, Search } from "lucide-react";
 import Image from "next/image";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import { Button } from "../ui/button";
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+  Accordion,
+} from "../ui/accordion";
 
 async function getMenu(type: string) {
   try {
@@ -108,13 +122,89 @@ export default async function Header() {
           </NavigationMenuList>
         </NavigationMenu>
         <form action="/search" className="ml-auto flex-1 max-w-sm">
-          <InputGroup className="">
+          <InputGroup className="h-10">
             <InputGroupInput placeholder="Tìm kiếm..." name="q" />
             <InputGroupAddon>
               <Search />
             </InputGroupAddon>
           </InputGroup>
         </form>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant={"outline"} className="md:hidden" size={"lg"}>
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>
+                <Image
+                  src="/logo.png"
+                  alt="Movie"
+                  width={48}
+                  height={48}
+                  loading="eager"
+                />
+              </SheetTitle>
+            </SheetHeader>
+            <nav className="flex flex-col items-start gap-5 px-4 pb-6 max-h-screen overflow-y-scroll">
+              <Accordion type="single" collapsible className="w-full space-y-5">
+                {categories.length ? (
+                  <AccordionItem value="the-loai">
+                    <AccordionTrigger className="justify-start text-base py-0">
+                      Thể loại
+                    </AccordionTrigger>
+                    <AccordionContent className="w-full mt-4 pl-2 pb-0">
+                      <div className="w-full grid grid-cols-2 gap-4">
+                        {categories.map((item: any) => (
+                          <Link
+                            key={item._id}
+                            href={`/the-loai/${item.slug}`}
+                            className="text-foreground/70 hover:text-primary"
+                          >
+                            {item.name}
+                          </Link>
+                        ))}
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
+                ) : null}
+                {categories.length ? (
+                  <AccordionItem value="quoc-gia">
+                    <AccordionTrigger className="justify-start text-base py-0">
+                      Quốc gia
+                    </AccordionTrigger>
+                    <AccordionContent className="w-full mt-4 pl-2 pb-0">
+                      <div className="w-full grid grid-cols-2 gap-4">
+                        {countries.map((item: any) => (
+                          <Link
+                            key={item._id}
+                            href={`/quoc-gia/${item.slug}`}
+                            className="text-foreground/70 hover:text-primary"
+                          >
+                            {item.name}
+                          </Link>
+                        ))}
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
+                ) : null}
+              </Accordion>
+              <Link
+                href="/danh-sach/phim-chieu-rap"
+                className="hover:text-primary"
+              >
+                Phim chiếu rạp
+              </Link>
+              <Link href="/danh-sach/phim-le" className="hover:text-primary">
+                Phim lẻ
+              </Link>
+              <Link href="/danh-sach/phim-bo" className="hover:text-primary">
+                Phim bộ
+              </Link>
+            </nav>
+          </SheetContent>
+        </Sheet>
       </section>
     </header>
   );
