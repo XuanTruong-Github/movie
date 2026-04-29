a3254090fafa8b2f8d60854a09c6d965d69fd017
truong-lx <truonglx@allinbyte.io>
2026-04-29 23:28:39 +0700

    updated

--- configs/api.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-export async function api(
-  url: string | URL | globalThis.Request,
-  options?: RequestInit,
-) {
-  const baseUrl = process.env.BASE_URL || "";
-  return fetch(`${baseUrl}/v1/api${url}`, options);
-}
