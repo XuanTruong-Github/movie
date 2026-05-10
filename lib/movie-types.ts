02e7be48482028b00efa4636828162da90522b86
truong-lx <truonglx@allinbyte.io>
2026-05-10 17:13:06 +0700

    updated

--- lib/movie-types.ts
+++
@@ -1,9 +0,0 @@
-export const movieTypes = [
-  { label: "Phim lẻ", slug: "phim-le" },
-  { label: "Phim bộ", slug: "phim-bo" },
-  { label: "Hoạt hình", slug: "hoat-hinh" },
-  { label: "Phim chiếu rạp", slug: "phim-chieu-rap" },
-  { label: "TV Shows", slug: "tv-shows" },
-  { label: "Vietsub", slug: "phim-vietsub" },
-  { label: "Thuyết minh", slug: "phim-thuyet-minh" },
-] as const;
